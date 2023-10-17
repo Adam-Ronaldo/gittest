9b046cf748b0928be5b66eb8446cd60064c4adfc
adam9 <adam9@DESKTOP-FJQL8H7>
2023-10-17 16:46:32 +0900

    ps_gitController tset메소드수정, msg메소드추가 tset문자열변수,선언 특정문구를 반환하는 msg메소드추가

--- gittest/src/com/gitest/controller/GitController.java
+++ gittest/src/com/gitest/controller/GitController.java
@@ -2,6 +2,9 @@ package com.gitest.controller;
 
 public class GitController {
 	public void test() {
-		System.out.println("test");
+		String name="ps";
+	}
+	public String msg() {
+		return "git재미있다";
 	}
 }
